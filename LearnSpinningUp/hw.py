d893994340b05ce65cf5cb00d321f2cef0f08616
plf <panlifan123@gmail.com>
2018-11-10 14:11:42 +0800

    添加了hw.py一些注释

--- LearnSpinningUp/hw.py
+++ LearnSpinningUp/hw.py
@@ -10,3 +10,9 @@ if __name__ == '__main__':
     logger_kwargs = dict(output_dir='path/to/output_dir', exp_name='experiment_name')  # 设置日志输出位置
 
     ppo(env_fn=env_fn, ac_kwargs=ac_kwargs, steps_per_epoch=5000, epochs=10, logger_kwargs=logger_kwargs)
+
+    # 使用 python -m spinup.run test_policy path/to/output_directory  可视化训练过程
+    # 使用 python -m spinup.run plot [path/to/output_directory/] [--legend [LEGEND ...]]
+    #     [--xaxis XAXIS] [--value [VALUE ...]] [--count] [--smooth S]
+    #     [--select [SEL ...]] [--exclude [EXC ...]]
+    # 绘图结果变化趋势
